4bb8178d8334ecfff53a085eb050f94f69354faf
evantwidwell <evanski@gmail.com>
2021-01-09 17:18:01 -0600

    finishing first little baby server

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const http = require('http');
 
 const hostname = 'localhost';
-
 const port = 4000;
 
 const server = http.createServer((req, res) => {
+  
+  const { url } = req;
+  console.log(url)
+  if(url ==='/translations'){
+    const translations = {1: 'one', 2: 'two', 3:'three'}
+    res.setHeader('Content-Type', 'application/json')
+
+    res.end(JSON.stringify(translations));
+  }
   res.end('Welcome to node')
 });
 
